2078816965fcc3c92be80e84eea86c6fa17a4644
Mark Abdilla Mifsud <60700422+MarkAbdillaMifsud@users.noreply.github.com>
2024-11-28 20:08:27 +0100

    Added bank accounts to user object

--- Console ATM SM4659/user.cpp
+++ Console ATM SM4659/user.cpp
@@ -1,3 +1,5 @@
+#include <memory>
+#include <iostream>
 #include "user.h"
 
 using namespace std;
@@ -18,4 +20,34 @@ void User::setUsername(const string& username) {
 
 void User::setPassword(const string& password){
     this->password = password;
+}
+
+void User::addBankAccount(shared_ptr<BankAccount> account){
+    int accountNumber = account->getAccountNumber();
+    if(bankAccounts.find(accountNumber) != bankAccounts.end()){
+        cerr << "An account with account number: " << accountNumber << " already exists." << endl;
+        return;
+    }
+
+    bankAccounts[accountNumber] = account;
+    cout << "Account with account number: " << accountNumber << " successfully created" << endl;
+}
+
+void User::removeBankAccount(int accountNumber){
+    if(bankAccounts.find(accountNumber) == bankAccounts.end()){
+        cerr << "An account with account number: " << accountNumber << " does not exist." << endl;
+        return;
+    }
+        bankAccounts.erase(accountNumber);
+        cout << "Account with account number: " << accountNumber << " successfully erased" << endl;
+}
+
+BankAccount* User::getBankAccount(int accountNumber){
+    auto it = bankAccounts.find(accountNumber);
+    if(it != bankAccounts.end()){
+        return it->second.get();
+    }
+
+    cerr << "An account with account number: " << accountNumber << " does not exist." << endl;
+    return nullptr;
 }
--- Console ATM SM4659/user.h
+++ Console ATM SM4659/user.h
@@ -1,5 +1,8 @@
 #pragma once
 #include <string>
+#include <unordered_map>
+#include <memory>
+#include "BankAccount.h"
 
 class User{
     public:
@@ -8,8 +11,14 @@ class User{
         std::string getPassword() const;
         void setUsername(const std::string& username);
         void setPassword(const std::string& password);
-    
+
+        void addBankAccount(std::shared_ptr<BankAccount> account);
+        void removeBankAccount(int accountNumber);
+        BankAccount* getBankAccount(int accountNumber);
+        std::unordered_map<std::string, std::shared_ptr<BankAccount>> getAllBankAccounts() const;
+
     private:
         std::string username;
         std::string password;
+        std::unordered_map<int, std::shared_ptr<BankAccount>> bankAccounts;
 };
